eaae7893799544e8fce90f54de9ce15418adcb1e
José Carlos <jcolscript@gmail.com>
2022-05-22 00:51:04 -0300

    refactor: refactor initial structure of project

--- second-part-explore-rijksmuseum/src/app/app.component.ts
+++ second-part-explore-rijksmuseum/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1 class="text-3xl font-bold underline">Hello world!</h1>`,
 })
 export class AppComponent {
   title = 'explore-rijksmuseum';
