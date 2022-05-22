ade3875c64bcb7ba6aa74a8d28672eb8c33832b6
José Carlos <jcolscript@gmail.com>
2022-05-22 00:42:39 -0300

    feat: add second part structure]

---
+++ second-part-explore-rijksmuseum/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
